705b70da9db4f094433b4cfc165911469ad158c6
ben <93186925+BenIzak@users.noreply.github.com>
2024-07-08 13:40:17 +0200

    boilerplate

--- client/App.tsx
+++ client/App.tsx
@@ -1,19 +1,48 @@
-import { StyleSheet, Text, View } from "react-native";
-import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function App() {
+import * as SplashScreen from "expo-splash-screen";
+import { View, ActivityIndicator, StyleSheet, Text } from "react-native";
+
+// Garder le splash screen visible pendant le chargement des ressources
+SplashScreen.preventAutoHideAsync();
+
+const Stack = createStackNavigator();
+
+const App: React.FC = () => {
+  const HomeScreen = () => {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Home</Text>
+      </View>
+    );
+  };
+
+  const ControlScreen = () => {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Control</Text>
+      </View>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Control"
+          component={ControlScreen}
+          options={{ title: "Car Control" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    flex: 1,
-    justifyContent: "center",
-  },
-});
+export default App;
