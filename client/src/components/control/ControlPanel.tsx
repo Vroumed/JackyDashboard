7b2b33d74eb8a169f354c4df2b55d0ddd24d8a42
ben izak <93186925+BenIzak@users.noreply.github.com>
2024-09-04 11:05:40 +0200

    refactor websocket management (#9)
    simplified use of the method
    improved readability

--- client/src/components/control/ControlPanel.tsx
+++ client/src/components/control/ControlPanel.tsx
@@ -1,18 +1,17 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import { View, StyleSheet } from "react-native";
 import ToolBar from "./ToolBar";
-import { HeadJoystick } from "./Joysticks/HeadJoystick";
-import { ControlJoystick } from "./Joysticks/ControlJoystick";
+import { HeadJoystick } from "./joysticks/HeadJoystick";
+import { ControlJoystick } from "./joysticks/ControlJoystick";
 import { useControlStore } from "@store/controlStore";
+import useWebSocketStore from "@store/useWebSocketStore";
 import useStore from "@store/useStore";
 import { IBackendCommand, IControlData, IHeadData } from "types/controls";
 
 const ControlPanel: React.FC = () => {
-    const { connectedCar } = useStore();
     const { controlData, headData } = useControlStore();
-    const [websocketStatus, setWebsocketStatus] = useState<string | null>(null);
-    const [_message, setMessage] = useState<string | null>(null);
-    const websocketRef = useRef<WebSocket | null>(null);
+    const { status, sendMessage } = useWebSocketStore();
+    const { connectedCar } = useStore();
 
     const controlDataRef = useRef<IControlData>(controlData);
     const headDataRef = useRef<IHeadData>(headData);
@@ -23,57 +22,24 @@ const ControlPanel: React.FC = () => {
     }, [controlData, headData]);
 
     useEffect(() => {
-        if (connectedCar) {
-            const ws = new WebSocket(`ws://${connectedCar.ip}/ws`);
-
-            ws.onopen = () => {
-                setWebsocketStatus("Connected");
-                console.log("WebSocket connected");
-            };
-
-            ws.onmessage = event => {
-                setMessage(event.data);
-                console.log("WebSocket message received:", event.data);
-            };
-
-            ws.onerror = error => {
-                setWebsocketStatus("Error");
-                console.error("WebSocket error:", error);
-            };
-
-            ws.onclose = () => {
-                setWebsocketStatus("Disconnected");
-                console.log("WebSocket disconnected");
-            };
-
-            websocketRef.current = ws;
-
-            return () => {
-                ws.close();
-                websocketRef.current = null;
-            };
-        }
-    }, [connectedCar]);
-
-    useEffect(() => {
-        if (websocketRef.current && websocketStatus === "Connected") {
+        if (status === "connected") {
             const interval = setInterval(() => {
                 const controlCommand: IBackendCommand<IControlData> = {
                     cmd: 1,
                     data: controlDataRef.current,
                 };
-                websocketRef.current?.send(JSON.stringify(controlCommand));
+                sendMessage(controlCommand);
 
                 const headCommand: IBackendCommand<IHeadData> = {
                     cmd: 2,
                     data: headDataRef.current,
                 };
-                websocketRef.current?.send(JSON.stringify(headCommand));
+                sendMessage(headCommand);
             }, 50);
 
             return () => clearInterval(interval);
         }
-    }, [websocketStatus])
+    }, [status, sendMessage])
 
     return (
         <View style={styles.container}>
@@ -85,19 +51,21 @@ const ControlPanel: React.FC = () => {
         </View>
     );
 };
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        justifyContent: "center",
+        alignItems: "center",
     },
     joystickContainer: {
-        width: '100%',
+        width: "100%",
         flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
+        flexDirection: "row",
+        justifyContent: "space-between",
         paddingHorizontal: 40,
-        alignItems: 'center',
+        alignItems: "center",
     },
 });
+
 export default ControlPanel;
--- client/src/store/useStore.ts
+++ client/src/store/useStore.ts
@@ -1,39 +1,48 @@
 import { create } from "zustand";
+import useWebSocketStore from "./useWebSocketStore";
 
 interface ConnectionState {
-  connectedCar: {
-    ip: string;
-    password: string;
-    name: string;
-    ready: boolean;
-  } | null;
-  status: "idle" | "loading" | "succeeded" | "failed";
-  error: string | null;
-  connectCar: (ip: string, password: string) => Promise<void>;
+    connectedCar: {
+        ip: string;
+        password: string;
+        name: string;
+        ready: boolean;
+    } | null;
+    status: "idle" | "loading" | "succeeded" | "failed";
+    error: string | null;
+    connectCar: (ip: string, password: string) => Promise<void>;
+    reconnectCar: () => Promise<void>;
 }
 
-const useStore = create<ConnectionState>(set => ({
-  connectedCar: null,
-  status: "idle",
-  error: null,
-  connectCar: async (ip, password) => {
-    set({ status: "loading" });
-    try {
-      const url = `http://${ip}/wrover`;
-      console.log("Connecting to car", url);
-      const response = await fetch(url, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      const data = await response.json();
-      console.log("Connected to car", data);
-      set({ connectedCar: { ...data, ip, password }, status: "succeeded" });
-    } catch (error) {
-      set({ error: (error as Error).message, status: "failed" });
-    }
-  },
+const useStore = create<ConnectionState>((set, get) => ({
+    connectedCar: null,
+    status: "idle",
+    error: null,
+    connectCar: async (ip, password) => {
+        set({ status: "loading" });
+        try {
+            const url = `http://${ip}/wrover`;
+            console.log("Connecting to car", url);
+            const response = await fetch(url, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+            const data = await response.json();
+            console.log("Connected to car", data);
+            set({ connectedCar: { ...data, ip, password }, status: "succeeded" });
+            useWebSocketStore.getState().connect(ip)
+        } catch (error) {
+            set({ error: (error as Error).message, status: "failed" });
+        }
+    },
+    reconnectCar: async () => {
+        const { connectedCar } = get();
+        if (connectedCar) {
+            await get().connectCar(connectedCar.ip, connectedCar.password);
+        }
+    },
 }));
 
 export default useStore;
