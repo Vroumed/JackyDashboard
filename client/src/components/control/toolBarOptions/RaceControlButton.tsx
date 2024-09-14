5bafd795c2e9f29ca63737095447752f34d28a68
Ewan Questier <96245296+xWane@users.noreply.github.com>
2024-09-14 16:24:44 +0200

    Jdb 29 implementation of api integration for command exchange and enhancements (#12)
    * add race control button to start and end a race, fix automode button in the control panel and not in the toolbar
    
    * add api call to start/end race button
    
    * fix type check errors
    
    ---------
    
    Co-authored-by: BenIzak <benizakahamadi@gmail.com>

---
+++ client/src/components/control/toolBarOptions/RaceControlButton.tsx
@@ -0,0 +1,47 @@
+import { endRace, startRace } from "@api/race";
+import React, { useState } from "react";
+import { Pressable, Text, StyleSheet } from "react-native";
+
+const RaceControlButton: React.FC = () => {
+  const [isRaceActive, setIsRaceActive] = useState(false);
+
+  const toggleRace = () => {
+    const newRaceState = !isRaceActive;
+
+    if (newRaceState) {
+      startRace();
+    } else {
+      endRace();
+    }
+
+    setIsRaceActive(newRaceState);
+  };
+
+  return (
+    <Pressable
+      onPress={toggleRace}
+      style={({ pressed }) => [styles.button, pressed && styles.buttonPressed]}
+    >
+      <Text style={styles.text}>
+        {isRaceActive ? "Stop Race" : "Start Race"}
+      </Text>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "#2196F3",
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonPressed: {
+    backgroundColor: "#1976D2",
+  },
+  text: {
+    color: "white",
+    fontWeight: "bold",
+  },
+});
+
+export default RaceControlButton;
