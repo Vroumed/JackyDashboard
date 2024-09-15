706984706dc8057a0bfe7da3dbf97f657a4a7e33
yasshabibi <108643485+yassbeats@users.noreply.github.com>
2024-09-15 14:28:22 +0200

    Enhance toast show function.

--- client/src/utility/toast.ts
+++ client/src/utility/toast.ts
@@ -1,6 +1,21 @@
 import Toast from "react-native-toast-message";
 
-const ToastShow = (message: string, type: string) => {
+declare type ToastType = "success" | "error" | "info";
+
+/**
+ * Show a toast message
+ * @param message - The message to display
+ * @param type - The type of the toast message
+ * @example
+ * ```ts
+ * ToastShow("This is a success message", "success");
+ * ```
+ */
+const ToastShow = (message: string, type: ToastType) => {
+  if (!message || !type) {
+    return;
+  }
+
   Toast.show({
     type: type,
     text1: type,
